javan/details-element-polyfill

commit 1d17534faea855b480a8f332430f784a8e9e06c7

CoffeeScript + Blade → JavaScript + Babel + Rollup + Karma
Fixes #12

src/index.js
@@ -1,0 +1,210 @@
+import support from "./support"
+import styles from "./styles"
+
+const { forEach, slice } = []
+
+if (!support.open) {
+  polyfillStyles()
+  polyfillProperties()
+  polyfillToggle()
+  polyfillAccessibility()
+}
+
+if (support.open && !support.toggle) {
+  polyfillToggleEvent()
+}
+
+function polyfillStyles() {
+  document.head.insertAdjacentHTML("afterbegin", `<style>${styles}</style>`)
+}
+
+function polyfillProperties() {
+  const { prototype } = document.createElement("details").constructor
+  const { setAttribute, removeAttribute } = prototype
+  const open = Object.getOwnPropertyDescriptor(prototype, "open")
+
+  Object.defineProperties(prototype, {
+    open: {
+      get() {
+        if (this.tagName == "DETAILS") {
+          return this.hasAttribute("open")
+        } else {
+          if (open && open.get) {
+            return open.get.call(this)
+          }
+        }
+      },
+      set(value) {
+        if (this.tagName == "DETAILS") {
+          return value
+            ? this.setAttribute("open", "")
+            : this.removeAttribute("open")
+        } else {
+          if (open && open.set) {
+            return open.set.call(this, value)
+          }
+        }
+      }
+    },
+    setAttribute: {
+      value(name, value) {
+        const call = () => { setAttribute.call(this, name, value) }
+        return this.tagName == "DETAILS"
+          ? triggerToggleIfToggled(this, call)
+          : call()
+      }
+    },
+    removeAttribute: {
+      value(name) {
+        const call = () => { removeAttribute.call(this, name) }
+        return this.tagName == "DETAILS"
+          ? triggerToggleIfToggled(this, call)
+          : call()
+      }
+    }
+  })
+}
+
+function polyfillToggle() {
+  onTogglingTrigger(element => {
+    const summary = element.querySelector("summary")
+    if (element.hasAttribute("open")) {
+      element.removeAttribute("open")
+      element.setAttribute("aria-expanded", false)
+    } else {
+      element.setAttribute("open", "")
+      element.setAttribute("aria-expanded", true)
+    }
+  })
+}
+
+function polyfillToggleEvent() {
+  if (window.MutationObserver) {
+    new MutationObserver(mutations => {
+      forEach.call(mutations, mutation => {
+        const { target, attributeName } = mutation
+        if (target.tagName == "DETAILS" && attributeName == "open") {
+          triggerToggle(toggle)
+        }
+      })
+    }).observe(document.documentElement, {
+      attributes: true,
+      subtree: true
+    })
+  } else {
+    onTogglingTrigger(element => {
+      const wasOpen = element.getAttribute("open")
+      setTimeout(() => {
+        const isOpen = element.getAttribute("open")
+        if (wasOpen != isOpen) {
+          triggerToggle(element)
+        }
+      }, 1)
+    })
+  }
+}
+
+function polyfillAccessibility() {
+  if (window.MutationObserver) {
+    new MutationObserver(mutations => {
+      forEach.call(mutations, mutation => {
+        forEach.call(mutation.addedNodes, setAccessibilityAttributes)
+      })
+    }).observe(document.documentElement, {
+      subtree: true,
+      childList: true
+    })
+  } else {
+    document.addEventListener("DOMNodeInserted", event => {
+      setAccessibilityAttributes(event.target)
+    })
+  }
+}
+
+function setAccessibilityAttributes(root) {
+  findElementsWithTagName(root, "SUMMARY").forEach(summary => {
+    const details = findClosestElementWithTagName(summary, "DETAILS")
+    summary.setAttribute("aria-expanded", details.hasAttribute("open"))
+    if (!summary.hasAttribute("tabindex"))
+      summary.setAttribute("tabindex", "0")
+    if (!summary.hasAttribute("role"))
+      summary.setAttribute("role", "button")
+  })
+}
+
+function eventIsSignificant(event) {
+  return !(
+    event.defaultPrevented ||
+    event.ctrlKey ||
+    event.metaKey ||
+    event.shiftKey ||
+    event.target.isContentEditable
+  )
+}
+
+function onTogglingTrigger(callback) {
+  addEventListener("click", event => {
+    if (eventIsSignificant(event)) {
+      if (event.which <= 1) {
+        const element = findClosestElementWithTagName(event.target, "SUMMARY")
+        if (element && element.parentNode && element.parentNode.tagName == "DETAILS") {
+          callback(element.parentNode)
+        }
+      }
+    }
+  }, false)
+
+  addEventListener("keydown", event => {
+    if (eventIsSignificant(event)) {
+      if (event.keyCode == 13 || event.keyCode == 32) {
+        const element = findClosestElementWithTagName(event.target, "SUMMARY")
+        if (element && element.parentNode && element.parentNode.tagName == "DETAILS") {
+          callback(element.parentNode)
+          event.preventDefault() // Prevent pagedown from space key
+        }
+      }
+    }
+  }, false)
+}
+
+function triggerToggle(element) {
+  const event = document.createEvent("Event")
+  event.initEvent("toggle", true, false)
+  element.dispatchEvent(event)
+}
+
+function triggerToggleIfToggled(element, callback) {
+  const wasOpen = element.getAttribute("open")
+  const result = callback()
+  const isOpen = element.getAttribute("open")
+  if (wasOpen != isOpen) {
+    triggerToggle(element)
+  }
+  return result
+}
+
+function findElementsWithTagName(root, tagName) {
+  return (
+    root.tagName == tagName
+      ? [root]
+      : []
+  ).concat(
+    typeof root.getElementsByTagName == "function"
+      ? slice.call(root.getElementsByTagName(tagName))
+      : []
+  )
+}
+
+function findClosestElementWithTagName(element, tagName) {
+  if (typeof element.closest == "function") {
+    return element.closest(tagName)
+  } else {
+    while (element) {
+      if (element.tagName == tagName) {
+        return element
+      } else {
+        element = element.parentNode
+      }
+    }
+  }
+}

test/index.js
@@ -1,0 +1,172 @@
+import "../src/index"
+const { module, test } = QUnit
+
+module("<details>", {
+  beforeEach() {
+    document.body.insertAdjacentHTML("beforeend", `
+      <div id="container">
+        <details id="details">
+          <summary id="summary">Summary</summary>
+          <div id="content">Content</div>
+        </details>
+      </div>
+    `)
+  },
+  afterEach() {
+    document.body.removeChild(document.getElementById("container"))
+  }
+})
+
+test("displays summary and hides content initially", (assert) => {
+  const done = assert.async()
+  defer(() => {
+    assert.notEqual(getElement("summary").offsetHeight, 0)
+    assert.equal(getElement("content").offsetHeight, 0)
+    done()
+  })
+})
+
+test("summary is focusable", (assert) => {
+  const done = assert.async()
+  const summary = getElement("summary")
+  defer(() => {
+    if (typeof HTMLDetailsElement === "undefined") {
+      assert.ok(summary.hasAttribute("tabindex"))
+      assert.ok(summary.hasAttribute("role"))
+    }
+    summary.focus()
+    assert.equal(document.activeElement, summary)
+    done()
+  })
+})
+
+test("open property toggles content", (assert) => {
+  const done = assert.async()
+
+  const element = getElement("details")
+  const content = getElement("content")
+
+  let toggleEventCount = 0
+  element.addEventListener("toggle", () => toggleEventCount++)
+
+  defer(() => {
+    element.open = true
+    defer(() => {
+      assert.notEqual(content.offsetHeight, 0)
+      assert.ok(element.hasAttribute("open"))
+      assert.ok(element.open)
+      assert.equal(toggleEventCount, 1)
+
+      element.open = false
+      defer(() => {
+        assert.equal(content.offsetHeight, 0)
+        assert.notOk(element.hasAttribute("open"))
+        assert.notOk(element.open)
+        defer(() => {
+          assert.equal(toggleEventCount, 2)
+          done()
+        })
+      })
+    })
+  })
+})
+
+test("open attribute toggles content", (assert) => {
+  const done = assert.async()
+
+  const element = getElement("details")
+  const content = getElement("content")
+
+  let toggleEventCount = 0
+  element.addEventListener("toggle", () => toggleEventCount++)
+
+  defer(() => {
+    element.setAttribute("open", "")
+    defer(() => {
+      assert.notEqual(content.offsetHeight, 0)
+      assert.equal(toggleEventCount, 1)
+
+      element.removeAttribute("open")
+      defer(() => {
+        assert.equal(content.offsetHeight, 0)
+        assert.equal(toggleEventCount, 2)
+        done()
+      })
+    })
+  })
+})
+
+test("click <summary> toggles content", (assert) => {
+  const done = assert.async()
+
+  const element = getElement("details")
+  const summary = getElement("summary")
+  const content = getElement("content")
+
+  let toggleEventCount = 0
+  element.addEventListener("toggle", () => toggleEventCount++)
+
+  defer(() =>
+    clickElement(summary, function() {
+      assert.notEqual(content.offsetHeight, 0)
+      assert.ok(element.hasAttribute("open"))
+      assert.equal(toggleEventCount, 1)
+
+      clickElement(summary, function() {
+        assert.equal(content.offsetHeight, 0)
+        assert.notOk(element.hasAttribute("open"))
+        assert.ok(toggleEventCount, 2)
+        done()
+      })
+    })
+  )
+})
+
+test("click <summary> child toggles content", (assert) => {
+  const done = assert.async()
+
+  const element = getElement("details")
+  const summary = getElement("summary")
+  const content = getElement("content")
+
+  const summaryChild = document.createElement("span")
+  summary.appendChild(summaryChild)
+
+  let toggleEventCount = 0
+  element.addEventListener("toggle", () => toggleEventCount++)
+
+  defer(() =>
+    clickElement(summaryChild, function() {
+      assert.notEqual(content.offsetHeight, 0)
+      assert.ok(element.hasAttribute("open"))
+      assert.equal(toggleEventCount, 1)
+
+      clickElement(summaryChild, function() {
+        assert.equal(content.offsetHeight, 0)
+        assert.notOk(element.hasAttribute("open"))
+        assert.ok(toggleEventCount, 2)
+        done()
+      })
+    })
+  )
+})
+
+function getElement(id) {
+  return document.getElementById(id)
+}
+
+function defer(callback) {
+  setTimeout(callback, 30)
+}
+
+function clickElement(element, callback) {
+  let event
+  try {
+    event = new MouseEvent("click", { view: window, bubbles: true, cancelable: true })
+  } catch (error) {
+    event = document.createEvent("MouseEvents")
+    event.initEvent("click", true, true)
+  }
+  element.dispatchEvent(event)
+  defer(callback)
+}

.babelrc
@@ -1,0 +1,11 @@
+{
+  "presets": [
+    [
+      "@babel/preset-env",
+      {
+        "modules": false,
+        "loose": true
+      }
+    ]
+  ]
+}

.blade.yml
@@ -1,28 +1,0 @@
-load_paths:
-  - src
-  - test/src
-
-logical_paths:
-  - details-element-polyfill.js
-  - test.js
-
-build:
-  logical_paths:
-    - details-element-polyfill.js
-  path: dist
-  js_compressor: uglifier
-
-plugins:
-  sauce_labs:
-    browsers:
-      Google Chrome:
-        version: -3
-      Firefox:
-        version: -3
-      Safari:
-        platform: Mac
-        version: -3
-      Microsoft Edge:
-        version: -2
-      Internet Explorer:
-        version: -2

.gitignore
@@ -1,1 +1,4 @@
+node_modules/
+test/dist/
 tmp/
+*.log

.ruby-version
@@ -1,1 +1,0 @@
-2.2.2

.travis.yml
@@ -1,10 +1,8 @@
-language: ruby
-cache: bundler
-branches:
-  only:
-    - master
+language: node_js
+node_js:
+  - "10"
+cache: yarn
 env:
   global:
     - SAUCE_USERNAME=details-polyfill
     - SAUCE_ACCESS_KEY=e1ee8b87-18e1-4ab4-b547-d716a485498b
-script: bin/blade ci

Gemfile
@@ -1,9 +1,0 @@
-source 'https://rubygems.org'
-
-gem 'blade'
-gem 'blade-sauce_labs_plugin'
-gem 'sprockets', '3.6.0'
-gem 'coffee-script'
-gem 'coffee-script-source'
-gem 'sass'
-gem 'uglifier', '2.7.2'

commit 1f4361ff7a66f21586020dfd1c70fb6d391f747a

Ensure initial <summary> elements are polyfilled
- Regression introduced in 1d17534faea855b480a8f332430f784a8e9e06c7
- Fixes test coverage added in 1eb7db8816ab7b6cb370b1054fee950fd9943467

src/index.js
@@ -105,6 +105,7 @@ function polyfillToggleEvent() {
 }
 
 function polyfillAccessibility() {
+  setAccessibilityAttributes(document)
   if (window.MutationObserver) {
     new MutationObserver(mutations => {
       forEach.call(mutations, mutation => {